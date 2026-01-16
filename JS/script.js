5a6c791def2ce8efa65bda5be1b7826df3e71ee7
javier garcia alvarez <javigarcial87@gmail.com>
2026-01-16 22:28:39 +0100

    cambios y correcciones

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ const btnUsers = document.getElementById('btnUsers')
 const btnBooks = document.getElementById('btnBooks')
 const content = document.getElementById('content')
 
-const API_URL = 'http://localhost:3002'
+const API_URL = 'http://localhost:4000'
 
 //USERS
 
