fc337efeeddc08ab8db2313baa17b670df94d915
javier garcia alvarez <javigarcial87@gmail.com>
2026-01-16 21:02:31 +0100

    obtener usuarios

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,54 @@ const btnBooks = document.getElementById('btnBooks')
 const content = document.getElementById('content')
 
 const API_URL = 'http://localhost:3000'
+
+//USERS
+
+btnUsers.addEventListener('click', async ()=>{
+    content.innerHTML = '<p>Cargando usuarios...</p>'
+
+    const response = await fetch (`${API_URL}/users`)
+    const users = await response.json()
+
+    content.innerHTML = users.map(user => `
+        
+        
+        
+        
+        
+        
+        
+        
+        `).join('')
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
