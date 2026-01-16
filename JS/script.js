23d95d640aa51812f51bb39addb4d5f40d1f027f
javier garcia alvarez <javigarcial87@gmail.com>
2026-01-16 20:51:59 +0100

    configurando front

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+const btnUsers = document.getElementById('btnUsers')
+const btnBooks = document.getElementById('btnBooks')
+const content = document.getElementById('content')
+
+const API_URL = 'http://localhost:3000'
